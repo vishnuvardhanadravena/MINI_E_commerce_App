b075402775d3f0d3342329a60c80cf232be2435d
vishnuvardhanadravena <vishnuvardhanadravena02@gmail.com>
2025-12-17 12:02:28 +0530

    editedvScreens1

--- lib/ui/SplashScreen.dart
+++ lib/ui/SplashScreen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:mini_e_commerce/ui/DashBorad.dart';
 import 'package:mini_e_commerce/ui/LoginScreen.dart';
 
 class SplashScreen extends StatefulWidget {
